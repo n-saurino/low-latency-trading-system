cf837db690cb3fdc45df314d3d8236faf0f24e06
Nigel Saurino <nigelsaurino@gmail.com>
2024-02-18 00:23:08 +0000

    Untrack build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/Github/low-latency-trading-system
-# Build directory: /home/pi/Github/low-latency-trading-system/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
