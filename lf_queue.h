d16798957f29c81a244ce808434fb7df72964460
Nigel Saurino <nigelsaurino@gmail.com>
2024-02-21 01:46:43 +0000

    Need to debug lock-free queue

--- lf_queue.h
+++ lf_queue.h
@@ -32,9 +32,23 @@ public:
         return &store[next_write_index_];
     }
 
+    auto GetNextToRead() noexcept{
+        return (next_read_index_ == next_write_index_ ? nullptr : &store[next_read_index_]);
+    }
+
+    auto Size() const noexcept{
+        return num_elements_.load();
+    }
+
     auto UpdateWriteIndex() noexcept{
         next_write_index_ = (next_write_index_ + 1) % store_.size();
         num_elements_++;
+    }
+
+    auto UpdateReadIndex(){
+        next_read_index_ = (next_read_index_ + 1) & store_.size();
+        ASSERT(num_elements_ != 0, "Read an invalid element in: " + std::to_string(pthread_self()));
+        num_elements_--;
     }
 };
 
