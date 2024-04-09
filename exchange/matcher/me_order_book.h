4aff7fe156e09cab9cf9fad156501b42d65c2397
Nigel Saurino <nigelsaurino@gmail.com>
2024-04-09 02:15:33 +0000

    Debugging the matching engine

--- exchange/matcher/me_order_book.h
+++ exchange/matcher/me_order_book.h
@@ -5,20 +5,32 @@
 #include "../../common/logging.h"
 #include "../order_server/client_response.h"
 #include "../market_data/market_update.h"
+#include "matching_engine.h"
 #include "me_order.h"
 
 using namespace Common;
 
 namespace Exchange{
 class MatchingEngine;
+
 class MEOrderBook final{
+public:
     // Add()
+    void Add(ClientId client_id, OrderId order_id, 
+            TickerId ticker_id, Side side, 
+            Price price, Qty qty){
+
+            }
 
     // Cancel()
+    void Cancel(ClientId client_id, OrderId order_id, TickerId ticker_id_){
+
+    }
 
     // MEOrderBook constructor
+    MEOrderBook(size_t, Logger*, Exchange::MatchingEngine*){
 
-    // Need a type for MatchingEngine::Start()
+    }
 };
 
 typedef std::array<MEOrderBook *, ME_MAX_TICKERS> OrderBookHashMap;
